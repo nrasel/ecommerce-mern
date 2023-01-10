bfea70a59539de13b134b9ca24af0ec02f61960c
nrasel <nrasel452@gmail.com>
2023-01-10 22:39:04 +0600

    home service

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -66,7 +66,7 @@ const Home = () => {
                     <h4>SUPERCHARGED FOR PROS.</h4>
                     <h5>iPad s13+ Pro.</h5>
                     <p>
-                      From $999.00 <br /> r $41.62/mo.
+                      From $999.00 <br /> or $41.62/mo.
                     </p>
                   </div>
                 </div>
@@ -85,6 +85,62 @@ const Home = () => {
                     </p>
                   </div>
                 </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="home-wrapper-2 py-5">
+        <div className="container">
+          <div className="row">
+            <div className="col-12">
+              <div className="services d-flex align-items-center justify-content-between">
+                <div className="d-flex align-items-center gap-10">
+                  <img src="/images/service.png" alt="" />
+                  <div>
+                    <h6>Free Shipping</h6>
+                    <p className="mb-0">From all orders over $5</p>
+                  </div>
+                </div>
+                <div className="d-flex align-items-center gap-10">
+                  <img src="/images/service-02.png" alt="" />
+                  <div>
+                    <h6>Daily Surprise Offers</h6>
+                    <p className="mb-0">Save upto 25% off </p>
+                  </div>
+                </div>
+                <div className="d-flex align-items-center gap-10">
+                  <img src="/images/service-03.png" alt="" />
+                  <div>
+                    <h6>Support 24/7</h6>
+                    <p className="mb-0">with an expert</p>
+                  </div>
+                </div>
+                <div className="d-flex align-items-center gap-10">
+                  <img src="/images/service-04.png" alt="" />
+                  <div>
+                    <h6>Affordable Prices</h6>
+                    <p className="mb-0">Get Factory Default Price</p>
+                  </div>
+                </div>
+                <div className="d-flex align-items-center gap-10">
+                  <img src="/images/service-05.png" alt="" />
+                  <div>
+                    <h6>Secure payment</h6>
+                    <p className="mb-0">100% protected payment</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="wrapper-3 py-5">
+        <div className="container">
+          <div className="row">
+            <div className="col-12">
+              <div className="categories">
+                
               </div>
             </div>
           </div>
