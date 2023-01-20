7852bd8fea146bc71c21361a55082a8540bd28c1
nrasel <nrasel452@gmail.com>
2023-01-20 23:56:05 +0600

    product details color size quantity

--- src/pages/SingleProduct.js
+++ src/pages/SingleProduct.js
@@ -3,6 +3,7 @@ import { Helmet } from "react-helmet";
 import ReactImageZoom from "react-image-zoom";
 import ReactStars from "react-rating-stars-component";
 import BreadCrumb from "../components/BreadCrumb";
+import Color from "../components/Color";
 import ProductCard from "../components/ProductCard";
 
 const SingleProduct = () => {
@@ -80,8 +81,70 @@ const SingleProduct = () => {
                     />
                     <p className="mb-0">(2 Reviews)</p>
                   </div>
+                  <a href="#review">Write a Review</a>
                 </div>
-                <a href="#review">Write a Review</a>
+                <div className="border-bottom py-3">
+                  <div className="d-flex mb-2 gap-10 align-items-center">
+                    <h3 className="product-heading">Type :</h3>
+                    <p className="product-data mb-0">Watch</p>
+                  </div>
+                  <div className="d-flex mb-2 gap-10 align-items-center">
+                    <h3 className="product-heading">Brand :</h3>
+                    <p className="product-data mb-0">Havels</p>
+                  </div>
+                  <div className="d-flex mb-2 gap-10 align-items-center">
+                    <h3 className="product-heading">Cateogry :</h3>
+                    <p className="product-data mb-0">Watch</p>
+                  </div>
+                  <div className="d-flex mb-2 gap-10 align-items-center">
+                    <h3 className="product-heading">Tags :</h3>
+                    <p className="product-data mb-0">Watch</p>
+                  </div>
+                  <div className="d-flex mb-2 gap-10 align-items-center">
+                    <h3 className="product-heading">Availability :</h3>
+                    <p className="product-data mb-0">In Stock</p>
+                  </div>
+                  <div className="d-flex gap-10 flex-column mb-3">
+                    <h3 className="product-heading">Size :</h3>
+                    <div className="d-flex flex-wrap gap-15">
+                      <span className="badge  border border-1 bg-white text-dark border-secondary">
+                        S
+                      </span>
+                      <span className="badge border border-1 bg-white text-dark border-secondary">
+                        M
+                      </span>
+                      <span className="badge border border-1 bg-white text-dark border-secondary">
+                        XL
+                      </span>
+                      <span className="badge border border-1 bg-white text-dark border-secondary">
+                        XXL
+                      </span>
+                    </div>
+                  </div>
+                  <div className="d-flex gap-10 flex-column mb-3">
+                    <h3 className="product-heading">Color :</h3>
+                    <Color />
+                  </div>
+                  <div className="d-flex gap-10 flex-row  align-items-center mb-3">
+                    <h3 className="product-heading">Quantity :</h3>
+                    <div className="">
+                      <input
+                        min={1}
+                        max={10}
+                        style={{ width: "50px" }}
+                        type="number"
+                      />
+                    </div>
+                    <div className="d-flex align-items-center gap-30 ms-5">
+                      <button className="button signup border-0">
+                        Add to Cart
+                      </button>
+                      <button type="submit" className="button border-0 ms-3">
+                        Buy It Now
+                      </button>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
